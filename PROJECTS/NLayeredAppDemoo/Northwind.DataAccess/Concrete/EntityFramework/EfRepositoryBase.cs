a777f9a8eeb7fd5df11732334492fd95ea9fe372
İsmail ERDOĞAN <113991303+isoreus@users.noreply.github.com>
2022-10-15 00:35:55 +0300

    Projects

---
+++ PROJECTS/NLayeredAppDemoo/Northwind.DataAccess/Concrete/EntityFramework/EfRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class,IEntity, new() // sınıf olmalı , IEnt. implement edilmeli ve yenilenmeki
+        where TContext:DbContext,new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity=context.Entry(entity); // ilgili nesneye eriş
+                addedEntity.State = EntityState.Added; // Abone ol nesneye , bulamayacaksın ve sen onu yeni ekle
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity); // ilgili nesneye eriş
+                deletedEntity.State = EntityState.Deleted; // Abone ol nesneye , bulamayacaksın ve sen onu yeni ekle
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using(TContext context = new TContext())
+            {
+                return filter==null?
+                    context.Set<TEntity>().ToList():
+                    context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity); // ilgili nesneye eriş
+                updatedEntity.State = EntityState.Modified; // Abone ol nesneye , bulamayacaksın ve update halde ol
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ PROJECTS/NLayeredAppDemoo/Northwind.WebFormsUI/Form1.cs
@@ -0,0 +1,169 @@
+﻿using Northwind.Bussines.Abstract;
+using Northwind.Bussines.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.DataAccess.Concrete.NHibernate;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Northwind.Bussines.DependencyResolvers.Ninject;
+using CSharpTest.Net.Interfaces;
+
+namespace Northwind.WebFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _productService = InstanceFactory.GetInstance<IProductService>();
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private IProductService _productService; 
+        private ICategoryService _categoryService;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ProductManager productManager = new ProductManager(new NhProductDal()); FARKLI BİR ŞEYLERİ LİSTELEYECEKSEN BÖYLE
+            // NH ve ENTİTY DİYE SOYALARDA OLUSTURDUK. DEĞİŞTİRMEK KOLAY OLUR PROJE AMAÇLI
+            LoadProduct();
+            LoadCategories(); // varmıs gibi kodladık sonra altına olusturdu
+        }
+
+        private void LoadCategories()
+        {
+            cbxCategory.DataSource = _categoryService.GetAll();
+            cbxCategory.DisplayMember = "CategoryName";
+            cbxCategory.ValueMember = "CategoryId";
+
+            cbxCategoryId.DataSource = _categoryService.GetAll();
+            cbxCategoryId.DisplayMember = "CategoryName";
+            cbxCategoryId.ValueMember = "CategoryId";
+
+            cbxCategoryUpdate.DataSource = _categoryService.GetAll();
+            cbxCategoryUpdate.DisplayMember = "CategoryName";
+            cbxCategoryUpdate.ValueMember = "CategoryId";
+        }
+
+        private void LoadProduct()
+        {
+            dgwProduct.DataSource = _productService.GetAll();
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dgwProduct.DataSource = _productService.GetProductsByCategory(Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+            catch (Exception)
+            {
+               
+            }
+        }
+
+        private void tbxProductName_TextChanged(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(tbxProductName.Text)) { // text içinde değer varsa ona göre getir
+            dgwProduct.DataSource=_productService.GetProductsByProductName(tbxProductName.Text);
+            }
+            else // eger değer yoksa
+            {
+                LoadProduct();
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productService.Add(new Product
+                {
+                    CategoryId = Convert.ToInt32(cbxCategoryId.SelectedValue),
+                    ProductName = tbxProductName2.Text,
+                    QuantityPerUnit = tbxQuantityPerUnit.Text,
+                    UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                    UnitsInStock = Convert.ToInt16(tbxStockAmount.Text)
+
+
+                });
+                MessageBox.Show("Ürün Veritabanınıza Eklendi");
+                LoadProduct();
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productService.Update(new Product
+                {
+                    ProductId = Convert.ToInt32(dgwProduct.CurrentRow.Cells[0].Value), // id direkt yukardan al
+                    ProductName = tbxUpdateProName.Text,
+                    CategoryId = Convert.ToInt32(cbxCategoryUpdate.SelectedValue),
+                    UnitsInStock = Convert.ToInt16(tbxStockUpdate.Text),
+                    QuantityPerUnit = tbxQuantityPerUpdate.Text,
+                    UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text),
+
+                });
+                MessageBox.Show("Ürün Veritabanınızda Güncellendi");
+                LoadProduct();
+
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void dgwProduct_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row=dgwProduct.CurrentRow;
+            tbxUpdateProName.Text=row.Cells[0].Value.ToString();
+            cbxCategoryUpdate.SelectedValue=row.Cells[2].Value;
+            tbxUnitPriceUpdate.Text=row.Cells[3].Value.ToString();
+            tbxQuantityPerUpdate.Text=row.Cells[4].Value.ToString();
+            tbxStockUpdate.Text=row.Cells[5].Value.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if(dgwProduct.CurrentRow != null) {
+                try
+                {
+                    _productService.Delete(new Product
+                    {
+                        ProductId = Convert.ToInt32(dgwProduct.CurrentRow.Cells[0].Value)
+                    });
+                    MessageBox.Show("Ürün Veritabanınızda SİLİNDİ");
+                    LoadProduct();
+                }
+                catch (Exception exception)
+                {
+
+                    MessageBox.Show(exception.Message);
+                }
+            }
+           
+        }
+    }
+}
